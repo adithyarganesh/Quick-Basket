5834d624ab4fbbd4100ae1207c897b51fb98782f
adithyarganesh <adithyarganesh@gmail.com>
2018-05-28 01:01:01 +0530

    Final commit

--- web page/getScreenshot.php
+++ web page/getScreenshot.php
@@ -2,9 +2,6 @@
  <?php
 
     include('D:/Quick Baket/web page/phpqrcode/qrlib.php');
-    <html>
-    <head>
-    <title> Helolo </title>
-    </head>   </html>
+ 
     QRcode::png('PHP QR Code :)'); 
 ?>
